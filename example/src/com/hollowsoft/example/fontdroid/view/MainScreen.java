ca36e30723b351a37707cb236b15c03cf3d4dbb2
Igor Morais <mor41s.1gor@gmail.com>
2014-07-25 22:19:11 -0300

    Major update

--- example/src/com/hollowsoft/example/fontdroid/view/MainScreen.java
+++ example/src/com/hollowsoft/example/fontdroid/view/MainScreen.java
@@ -17,21 +17,72 @@
 
 package com.hollowsoft.example.fontdroid.view;
 
-import com.hollowsoft.example.fontdroid.R;
-
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+
+import com.hollowsoft.example.fontdroid.R;
 
 /**
  * @author Igor Morais
  * @author Mor41s.1gor@gmail.com
  */
-public final class MainScreen extends Activity {
+public final class MainScreen extends FragmentActivity implements TabListener, OnPageChangeListener {
+
+    private ActionBar actionBar;
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(final Bundle savedInstance) {
         super.onCreate(savedInstance);
 
-        setContentView(R.layout.digital_clock);
+        setContentView(R.layout.main_screen);
+
+        actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        viewPager = (ViewPager) findViewById(R.id.main_screen_menu_view_pager);
+        viewPager.setAdapter(new MenuAdapter(getSupportFragmentManager(), this));
+        viewPager.setOnPageChangeListener(this);
+
+        for (final String name : getResources().getStringArray(R.array.menu)) {
+            actionBar.addTab(actionBar.newTab().setText(name).setTabListener(this));
+        }
+    }
+
+    @Override
+    public void onTabReselected(final Tab tab, final FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabSelected(final Tab tab, final FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(final Tab tab, final FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(final int state) {
+
+    }
+
+    @Override
+    public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(final int position) {
+        actionBar.setSelectedNavigationItem(position);
     }
 }
---
+++ example/src/com/hollowsoft/example/fontdroid/view/MenuAdapter.java
@@ -0,0 +1,92 @@
+package com.hollowsoft.example.fontdroid.view;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MenuAdapter extends FragmentPagerAdapter {
+
+    private static final int AMOUNT = 13;
+
+    private final Context context;
+
+    public MenuAdapter(final FragmentManager fragmentManager, final Context context) {
+        super(fragmentManager);
+
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return AMOUNT;
+    }
+
+    @Override
+    public Fragment getItem(final int position) {
+        Fragment fragment;
+
+        switch (position) {
+
+        case 0:
+            fragment = AutoCompleteTextViewFragment.instantiate(context,
+                    AutoCompleteTextViewFragment.class.getName());
+            break;
+
+        case 1:
+            fragment = ButtonFragment.instantiate(context, ButtonFragment.class.getName());
+            break;
+
+        case 2:
+            fragment = CheckBoxFragment.instantiate(context, CheckBoxFragment.class.getName());
+            break;
+
+        case 3:
+            fragment = CheckedTextViewFragment.instantiate(context, CheckedTextViewFragment.class.getName());
+            break;
+
+        case 4:
+            fragment = ChronometerFragment.instantiate(context, ChronometerFragment.class.getName());
+            break;
+
+        case 5:
+            fragment = DigitalClockFragment.instantiate(context, DigitalClockFragment.class.getName());
+            break;
+
+        case 6:
+            fragment = EditTextFragment.instantiate(context, EditTextFragment.class.getName());
+            break;
+
+        case 7:
+            fragment = MultiAutoCompleteTextViewFragment.instantiate(context,
+                    MultiAutoCompleteTextViewFragment.class.getName());
+            break;
+
+        case 8:
+            fragment = RadioButtonFragment.instantiate(context, RadioButtonFragment.class.getName());
+            break;
+
+        case 9:
+            fragment = SwitchFragment.instantiate(context, SwitchFragment.class.getName());
+            break;
+
+        case 10:
+            fragment = TextClockFragment.instantiate(context, TextClockFragment.class.getName());
+            break;
+
+        case 11:
+            fragment = TextViewFragment.instantiate(context, TextViewFragment.class.getName());
+            break;
+
+        case 12:
+            fragment = ToggleButtonFragment.instantiate(context, ToggleButtonFragment.class.getName());
+            break;
+
+        default:
+            fragment = ButtonFragment.instantiate(context, ButtonFragment.class.getName());
+            break;
+        }
+
+        return fragment;
+    }
+}
