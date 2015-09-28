02853e446be6e7bd9947f8d8b8b38bd99e6f0b80
Igor Morais <mor41s.1gor@gmail.com>
2015-09-28 01:30:06 -0300

    Add Switch

---
+++ FontDroid/app/src/main/java/hollowsoft/fontdroid/Switch.java
@@ -0,0 +1,57 @@
+package hollowsoft.fontdroid;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.util.AttributeSet;
+
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public class Switch extends android.widget.Switch implements IFont {
+
+    public Switch(final Context context) {
+        super(context);
+    }
+
+    public Switch(final Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+
+        getStyleable(context, attributeSet, 0, 0);
+    }
+
+    public Switch(final Context context, final AttributeSet attributeSet, final int defStyleAttr) {
+        super(context, attributeSet, defStyleAttr);
+
+        getStyleable(context, attributeSet, defStyleAttr, 0);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public Switch(final Context context, final AttributeSet attributeSet, final int defStyleAttr, final int defStyleRes) {
+        super(context, attributeSet, defStyleAttr, defStyleRes);
+
+        getStyleable(context, attributeSet, defStyleAttr, defStyleRes);
+    }
+
+    private void getStyleable(final Context context, final AttributeSet attributeSet, final int defStyleAttr, final int defStyleRes) {
+
+        final TypedArray typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.Font, defStyleAttr, defStyleRes);
+
+        if (!isInEditMode()) {
+
+            final String fontPath = typedArray.getString(R.styleable.Font_fontPath);
+
+            setFont(fontPath);
+        }
+
+        typedArray.recycle();
+    }
+
+    @Override
+    public void setFont(final String path) {
+
+        final Typeface typeface = FontCache.with().put(path, getContext().getAssets());
+
+        setTypeface(typeface);
+    }
+}
